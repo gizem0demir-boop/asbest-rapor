64209446ae3d8e3eaf99c7ee528777b5a7953abc
gizem0demir-boop <gizem0demir@gmail.com>
2026-08-26 23:34:01 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,7 @@
 # utils/__init__.py
+import os
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
 
 def parse_asbest_tutanak(*args, **kwargs):
     return {}
@@ -8,3 +11,6 @@ def generate_bolum_summary(*args, **kwargs):
 
 def process_and_get_image(*args, **kwargs):
     return None
+
+def read_tutanak_details(*args, **kwargs):
+    return {}

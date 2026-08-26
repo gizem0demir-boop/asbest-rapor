22169cee7a405ce390c3d6d6b5cfad8777e74262
gizem0demir-boop <gizem0demir@gmail.com>
2026-08-26 23:31:52 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,1 +1,10 @@
-from .excel_parser import *
+# utils/__init__.py
+
+def parse_asbest_tutanak(*args, **kwargs):
+    return {}
+
+def generate_bolum_summary(*args, **kwargs):
+    return ""
+
+def process_and_get_image(*args, **kwargs):
+    return None

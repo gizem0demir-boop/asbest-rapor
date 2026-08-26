34d4556e0a384804843526da58dc503877f30e67
gizem0demir-boop <gizem0demir@gmail.com>
2026-08-26 23:25:27 +0300

    Update yikim_plani_modulu.py

--- modules/yikim_plani_modulu.py
+++ modules/yikim_plani_modulu.py
@@ -1,3 +1,11 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import pandas as pd
+import streamlit as st
+from docxtpl import DocxTemplate
+from utils.excel_parser import read_tutanak_details
 import os
 import datetime
 import pandas as pd

ccbd04a3807b4aa9812f86baadd19be2df4d1234
gizem0demir-boop <gizem0demir@gmail.com>
2026-08-26 23:41:32 +0300

    Update __init__.py

--- utils/__init__.py
+++ utils/__init__.py
@@ -5,8 +5,13 @@ from .excel_parser import read_tutanak_details
 UPLOAD_FOLDER = os.path.join(os.getcwd(), "uploads")
 
 def parse_asbest_tutanak(uploaded_file):
-    # excel_parser.py içindeki gerçek okuma fonksiyonuna yönlendirir
-    return read_tutanak_details(uploaded_file)
+    # excel_parser'dan dönen veriyi alır
+    res = read_tutanak_details(uploaded_file)
+    if isinstance(res, tuple) and len(res) == 2:
+        return res
+    elif isinstance(res, dict):
+        return res, res.get("samples", [])
+    return {}, []
 
 def generate_bolum_summary(*args, **kwargs):
     return ""
